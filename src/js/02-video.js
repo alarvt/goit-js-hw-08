188198da6b3be5c9b32a8f665cade2a2742946f0
alarvt <alarvt@gmail.com>
2022-12-04 10:00:56 +0100

    Проблеми

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,44 +2,22 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 
-/* const player = new Player('iframe', {}); */
+const player = new Player('iframe');
 
-iframe.addEventListener('timeupdate', event => {
-  console.log('The currentTime attribute has been updated. Again.');
-});
-
-/* player.on('eventName', function (data) {
-  // data is an object containing properties specific to that event
-  localStorage.setItem("timeupdate");
-}); */
-
-/* const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
- */
-
-/* const onPlay = function (data) {
-  // data is an object containing properties specific to that event
-  onPlay.addEventListener('timeupdate', e => {
-    console.log('iframe', e.target.value);
-  });
+const onPlay = function (data) {
+  
 };
 
 player.on('play', onPlay);
 
 
-player.on('timeupdate', function (data) {
+
+
+/* player.on('timeupdate', function (data) {
   console.log(timeupdate);
   // data is an object containing properties specific to that event
-});
- */
+}); */
+
 
 /* player
   .setCurrentTime()

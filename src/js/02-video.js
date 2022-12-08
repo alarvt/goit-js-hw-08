db297386824aa4e055187049be8e2f771eaa4e5c
alarvt <alarvt@gmail.com>
2022-12-08 10:34:30 +0100

    ДЗ-8.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,37 +1,18 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 
-const player = new Player('iframe');
+const player = new Player(iframe);
 
-const onPlay = function (data) {
-  
+const onPlay = function ( { seconds }) {
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
 };
 
-player.on('play', onPlay);
 
 
+player.setCurrentTime(localStorage.getItem('setCurrentTime(|)')||0);
 
+player.on('timeupdate', throttle(onPlay, 1000));
 
-/* player.on('timeupdate', function (data) {
-  console.log(timeupdate);
-  // data is an object containing properties specific to that event
-}); */
-
-
-/* player
-  .setCurrentTime()
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  }); */
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,54 @@
 
-const taskInputEmail = document.querySelector('.task__email');
-const taskInputMessage = document.querySelector('.task__message');
+import throttle from "lodash.throttle";
 
-taskInputEmail.addEventListener('keydown', e => {
-  localStorage.setItem('task__email', e.target.value);
-  console.log('task__email', e.target.value);
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+
+form.addEventListener('input', e => {
+  localStorage.setItem('feedback-form-state', email);
+
 });
 
-taskInputMessage.addEventListener('keydown', e => {
-  localStorage.setItem('task__message', e.target.value);
-  console.log('task__message', e.target.value);
+form.addEventListener('submit', e => {
+  localStorage.setItem('feedback-form-state', textarea);
+  console.log('email', 'message');
 });
 
-localStorage.setItem('feedback-form-state', 'submit');
+
+/* import throttle from 'lodash.throttle'; */
+
+/* const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+populateData();
+function populateData() {
+  const parseData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if (parseData) {
+    console.log(parseData);
+    refs.textarea.value = parseData.message;
+    refs.email.value = parseData.email;
+  }
+} */
